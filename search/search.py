3434101067b835d78fe5367d6a5564c5d702aba4
Thomas Wheeler <thomassw66@gmail.com>
2017-09-09 12:38:02 -0700

    Project 1 Search q1-q4

--- search/search.py
+++ search/search.py
@@ -18,6 +18,7 @@ Pacman agents (in searchAgents.py).
 """
 
 import util
+from collections import defaultdict
 
 class SearchProblem:
     """
@@ -72,6 +73,60 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
+class Node:
+    """
+        Nodes contain state as well info about parent and action taken from that parent
+        When a solution is found we can call path to generate the list of actions it took 
+        to reach this node.
+    """
+    def __init__(self, state, parent=None, action=None, pathcost=0, depth=0):
+        # initialize
+        self.state = state
+        self.parent = parent
+        self.action = action 
+        self.pathcost = pathcost 
+        if parent: 
+            self.depth = parent.depth + 1
+        else:
+            self.depth = depth 
+
+    def __repr__(self):
+        return "%s" % self.state
+
+    def path(self):
+        """
+        Nodes form a linked list data structure. 
+            To generate the actions to a node we traverse the list, adding actions to the path
+            until we reach the root node (whose parent will be None)
+        """
+        x, actions = self, []
+        while x.parent:
+            actions.insert(0, x.action)
+            x = x.parent
+        return actions
+
+def genericGraphSearch(problem, frontier):
+    visited = {}
+    frontier.push(Node(problem.getStartState()))
+    while frontier:
+        node = frontier.pop()
+        if problem.isGoalState(node.state):
+            return node.path()
+        """ 
+        IMPORTANT DISTINCTION
+         We ONLY expand nodes that have not been visited,
+         but we add all the nodes reachable from node to the frontier (regardless of whether or not theyve been visited)
+            I spent a lot of time scratching my head about this... 
+            there may be visited nodes in the frontier
+         """
+        if node.state not in visited:
+            visited[node.state] = True
+            for state, act, step in problem.getSuccessors(node.state):
+                n = Node(state, node, act, node.pathcost + step)
+                frontier.push(n)
+    return None
+
+
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
@@ -81,23 +136,27 @@ def depthFirstSearch(problem):
 
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
-
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
+    # print "Start:", problem.getStartState()
+    # print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    # print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return genericGraphSearch(problem, util.Stack())
+
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return genericGraphSearch(problem, util.Queue())
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    """
+        Uniform Cost Search is the same as A* search with f(n) = g(n)
+    """
+    return aStarSearch(problem, nullHeuristic)
 
 def nullHeuristic(state, problem=None):
     """
@@ -109,7 +168,7 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return genericGraphSearch(problem, util.PriorityQueueWithFunction(lambda n: heuristic(n.state, problem) + n.pathcost))
 
 
 # Abbreviations
